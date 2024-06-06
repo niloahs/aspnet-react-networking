49ea3e36802e02e41124a018db032f3f29275b47
Mark <markmekhail@pm.me>
2024-06-06 17:20:03 -0400

    added axios and hooked up all requests to the api

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,15 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({
+                                         activity: selectedActivity,
+                                         closeForm,
+                                         createOrEdit,
+                                         submitting
+                                     }: Props) {
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -40,14 +46,14 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                                name={'description'} onChange={handleInputChange}/>
                 <Form.Input placeholder='Category' value={activity.category} name={'category'}
                             onChange={handleInputChange}/>
-                <Form.Input placeholder='Date' value={activity.date} name={'date'}
+                <Form.Input type={'date'} placeholder='Date' value={activity.date} name={'date'}
                             onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name={'city'}
                             onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name={'venue'}
                             onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
-                <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
+                <Button loading={submitting} onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
     )
