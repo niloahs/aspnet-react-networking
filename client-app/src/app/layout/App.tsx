13969473ace6b523081598f2bd49c04d6c2d726d
Mark <markmekhail@pm.me>
2024-06-13 13:43:07 -0400

    added validation and exception handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,14 @@ import NavBar from "./NavBar.tsx";
 import { observer } from "mobx-react-lite";
 import { Outlet, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage.tsx";
+import { ToastContainer } from "react-toastify";
 
 function App() {
     const location = useLocation();
 
     return (
         <>
+            <ToastContainer position='bottom-right' hideProgressBar={true} theme={'colored'} />
             {location.pathname === '/' ? <HomePage /> : (
                 <>
                     <NavBar />
@@ -20,6 +22,5 @@ function App() {
         </>
     );
 }
-
 
 export default observer(App);
